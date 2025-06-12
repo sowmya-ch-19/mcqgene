f6437f19b973fc59d97ba9d2d9246b30a43f514c
Sowmya Cherukupally <xyugsowmya19@gmail.com>
2025-06-12 06:41:50 +0000

    changes are done

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -21,7 +20,8 @@ select *
 from source_data
 
 /*
-    Uncomment the line below to remove records with null `id` values
+    Uncomment the line below to remove records with null id values
 */
 
--- where id is not null
+where id is not null
+
